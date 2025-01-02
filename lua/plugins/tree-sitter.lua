0de7a32c5ce09e71b850e127f5ecc76597483ada
pekochan069 <pekochan069@gmail.com>
2025-01-03 00:07:13 +0900

    Migrate to LazyVim

--- lua/plugins/tree-sitter.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-        ensure_update = { "c", "cpp", "gitignore", "json", "lua", "markdown" },
-        auto_install = true,
-    },
-}
